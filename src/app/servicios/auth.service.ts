88adaad4bc0e04982c23348c51835d3655e7a656
damik07 <96784561+damik07@users.noreply.github.com>
2023-03-14 09:20:26 -0300

    7 files added

---
+++ src/app/servicios/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JwtDto } from './models/jwt-dto';
+import { LoginUsuario } from './models/login-usuario';
+import { NuevoUsuario } from './models/nuevo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  api = "https://nameless-springs-70391.herokuapp.com/auth/"; //hay que cambiar l dirección cuando arme el backend
+
+  constructor(private http:HttpClient) { }
+
+  Login(credenciales:LoginUsuario):Observable<JwtDto> {
+    
+    return this.http.post<JwtDto>(this.api + `login`, credenciales)
+   
+  }
+
+  Nuevo(nuevoUsuario:NuevoUsuario):Observable<any> {
+    return this.http.post(this.api + `nuevoUsu`, nuevoUsuario)
+   
+  }
+  
+  public get logIn(): boolean {
+    return (sessionStorage.getItem('currentUser') == "true"); //tenia registrado un == !null
+  }
+
+}
---
+++ src/app/servicios/guard/guard.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { TokenService } from '../token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+
+  realRole!: string;
+
+  constructor(private authService:AuthService, private ruta:Router, private token:TokenService){}
+  canActivate(
+    route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+      //let currentUser=this.authService.UsuarioAutenticado; texto de luisina en las masterclass
+      //if (currentUser && currentUser.accessToken){
+      //  return true;
+      //}
+      //else {
+      //  this.ruta.navigate(['/iniciar-sesion']);
+      //  return false;
+      //}
+
+      //const expectedRol = route.data.expectedRol;
+      const roles = this.token.getAuthorities();
+      this.realRole = 'user';
+      roles.forEach(rol=> {
+        if(rol === 'ROLE_ADMIN'){
+        this.realRole = 'admin';
+        }
+      });
+      if(!this.token.getToken()){
+        this.ruta.navigate(['/']);
+        return false;
+
+      }
+
+      return true;
+  }
+}
