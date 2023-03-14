550d96f719406f309d0209819f0305f229aef25c
damik07 <96784561+damik07@users.noreply.github.com>
2023-03-14 08:16:25 -0300

    10 files added

---
+++ src/app/plan-cuentas/cuenta-contable/cuenta-contable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cuenta-contable',
+  templateUrl: './cuenta-contable.component.html',
+  styleUrls: ['./cuenta-contable.component.css']
+})
+export class CuentaContableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/plan-cuentas/sub-rubro/sub-rubro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-rubro',
+  templateUrl: './sub-rubro.component.html',
+  styleUrls: ['./sub-rubro.component.css']
+})
+export class SubRubroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
