0bcd9cf54ff28c50b6a883e0949a6d071b46d10f
damik07 <96784561+damik07@users.noreply.github.com>
2023-03-23 10:54:00 -0300

    1 file modified

--- src/app/servicios/serviciosContables/rubros-contables.service.ts
+++ src/app/servicios/serviciosContables/rubros-contables.service.ts
@@ -5,6 +5,55 @@ import { Injectable } from '@angular/core';
 export class RubrosContablesService {
   
   rubrosContables?:any=[
+    {
+      "codigoRubro": "1",
+      "nombreRubro": "Activo",
+      "Nivel": "1",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": ""
+    },
+    {
+      "codigoRubro": "01",
+      "nombreRubro": "Caja y Banco",
+      "Nivel": "2",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": "Activo"
+    },
+    {
+      "codigoRubro": "01",
+      "nombreRubro": "Caja",
+      "Nivel": "3",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": "Caja y Banco"
+    },
+    {
+      "codigoRubro": "02",
+      "nombreRubro": "Banco BERSA c/c",
+      "Nivel": "3",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": "Caja y Banco"
+    },
+    {
+      "codigoRubro": "02",
+      "nombreRubro": "Deudores por ventas",
+      "Nivel": "2",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": "Activo"
+    },
+    {
+      "codigoRubro": "01",
+      "nombreRubro": "Deudores por ventas",
+      "Nivel": "3",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": "Deudores por ventas"
+    },
+    {
+      "codigoRubro": "02",
+      "nombreRubro": "Deudores morosos",
+      "Nivel": "3",
+      "tipoCuenta": "Activo",
+      "rubroSuperior": "Deudores por ventas"
+    }
     
   ];
 
