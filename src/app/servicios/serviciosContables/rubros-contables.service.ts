0c9f77ebaff2d4f34d9f8d29a7ef833d293300e4
damik07 <96784561+damik07@users.noreply.github.com>
2023-03-27 12:14:14 -0300

    1 file modified

--- src/app/servicios/serviciosContables/rubros-contables.service.ts
+++ src/app/servicios/serviciosContables/rubros-contables.service.ts
@@ -11,49 +11,56 @@ export class RubrosContablesService {
       "nombreRubro": "Activo",
       "Nivel": "1",
       "tipoCuenta": "Activo",
-      "rubroSuperior": ""
+      "rubroSuperior": "",
+      "codificacion": "1.00.00.00"
     },
     {
       "codigoRubro": "01",
       "nombreRubro": "Caja y Banco",
       "Nivel": "2",
       "tipoCuenta": "Activo",
-      "rubroSuperior": "Activo"
+      "rubroSuperior": "Activo",
+      "codificacion": "1.01.00.00"
     },
     {
       "codigoRubro": "01",
       "nombreRubro": "Caja",
       "Nivel": "3",
       "tipoCuenta": "Activo",
-      "rubroSuperior": "Caja y Banco"
+      "rubroSuperior": "Caja y Banco",
+      "codificacion": "1.01.01.00"
     },
     {
       "codigoRubro": "02",
       "nombreRubro": "Banco BERSA c/c",
       "Nivel": "3",
       "tipoCuenta": "Activo",
-      "rubroSuperior": "Caja y Banco"
+      "rubroSuperior": "Caja y Banco",
+      "codificacion": "1.01.02.00"
     },
     {
       "codigoRubro": "02",
       "nombreRubro": "Deudores por ventas",
       "Nivel": "2",
       "tipoCuenta": "Activo",
-      "rubroSuperior": "Activo"
+      "rubroSuperior": "Activo",
+      "codificacion": "1.02.00.00"
     },
     {
       "codigoRubro": "01",
       "nombreRubro": "Deudores por ventas",
       "Nivel": "3",
       "tipoCuenta": "Activo",
-      "rubroSuperior": "Deudores por ventas"
+      "rubroSuperior": "Deudores por ventas",
+      "codificacion": "1.02.01.00"
     },
     {
       "codigoRubro": "02",
       "nombreRubro": "Deudores morosos",
       "Nivel": "3",
       "tipoCuenta": "Activo",
-      "rubroSuperior": "Deudores por ventas"
+      "rubroSuperior": "Deudores por ventas",
+      "codificacion": "1.02.02.00"
     }
     
   ];
