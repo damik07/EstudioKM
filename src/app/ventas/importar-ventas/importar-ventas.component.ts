d7e120478c22439728240b0f5278deab555b2065
damik07 <96784561+damik07@users.noreply.github.com>
2023-04-05 08:52:36 -0300

    1 file modified

--- src/app/ventas/importar-ventas/importar-ventas.component.ts
+++ src/app/ventas/importar-ventas/importar-ventas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { CuentasContablesService } from '../../servicios/serviciosContables/cuentas-contables.service';
+
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -11,8 +13,14 @@ export class ImportarVentasComponent implements OnInit {
 
   public files: NgxFileDropEntry[] = [];
   data?:any[];
+  cuenta?:any[];
+  facturas?:any[] = [];
+  fechaDeInicio: string;
 
-  constructor() { }
+  constructor(private cuentas:CuentasContablesService) {
+    this.cuenta = cuentas.cuentasContables;
+    console.log(this.cuenta);
+   }
 
   ngOnInit() {
   }
@@ -82,6 +90,44 @@ export class ImportarVentasComponent implements OnInit {
     reader.readAsBinaryString(file);
   }
 
+  obtenerValoresDeTabla() {
+    const tabla = document.getElementById('importFactCompras');
+    const filas = tabla.getElementsByTagName('tr');
+    for (let i = 1; i < filas.length; i++) {
+      const celdas = filas[i].getElementsByTagName('td');
+      const objetoDeFila = {
+        fecha_factura: celdas[0].innerHTML,
+        documento: celdas[1].innerHTML,
+        p_venta: celdas[2].innerHTML,
+        n_desde: celdas[3].innerHTML,
+        n_hasta: celdas[4].innerHTML,
+        cod_autoriz: celdas[5].innerHTML,
+        doc_emisor: celdas[6].innerHTML,
+        n_emisor: celdas[7].innerHTML,
+        denominacion: celdas[8].innerHTML,
+        tc: celdas[9].innerHTML,
+        moneda: celdas[10].innerHTML,
+        neto_gravado: celdas[11].innerHTML,
+        neto_no_gravado: celdas[12].innerHTML,
+        op_exentas: celdas[13].innerHTML,
+        iva: celdas[14].innerHTML,
+        total: celdas[15].innerHTML,
+        cuenta: celdas[16].querySelector('select').value,
+        fecha_imputacion: this.fechaDeInicio,
+        fecha_carga: new Date()
+        
+      };
+           
+      this.facturas.push(objetoDeFila);
+      console.log(this.facturas);
+    }
+  }
+
+
+  guardarBD() {
+    console.log("agregar función POST luego de crear el backend, en data está la información, falta agregar la fecha")
+    
+  }
 
 
 
