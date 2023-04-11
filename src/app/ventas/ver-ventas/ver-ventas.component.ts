c2b50359c8b711a328d80a4154d47c853d96f3ea
damik07 <96784561+damik07@users.noreply.github.com>
2023-04-11 11:07:40 -0300

    2 files modified

--- src/app/ventas/ver-ventas/ver-ventas.component.ts
+++ src/app/ventas/ver-ventas/ver-ventas.component.ts
@@ -20,17 +20,20 @@ export class VerVentasComponent implements OnInit {
     // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
     const ventas = this.ventas.facturasVentas;
     const datosFiltrados1 = ventas.filter(dato => 
-      new Date(dato.fecha_comprobante).getMonth() >= new Date(formData.startDate1).getMonth()
+      new Date(dato.fecha_imputacion).getMonth() >= new Date(formData.startDate1).getMonth()
     );
     const dfiltro1 = datosFiltrados1.filter(dato =>
-      new Date(dato.fecha_comprobante).getFullYear() >= new Date(formData.startDate1).getFullYear()
+      new Date(dato.fecha_imputacion).getFullYear() >= new Date(formData.startDate1).getFullYear()
     );
     const datosFiltrados2 = dfiltro1.filter(dato => 
-      new Date(dato.fecha_comprobante).getMonth() <= new Date(formData.endDate1).getMonth()
+      new Date(dato.fecha_imputacion).getMonth() <= new Date(formData.endDate1).getMonth()
     );
     this.filtro = datosFiltrados2.filter(dato =>
-      new Date(dato.fecha_comprobante).getFullYear() <= new Date(formData.endDate1).getFullYear()
+      new Date(dato.fecha_imputacion).getFullYear() <= new Date(formData.endDate1).getFullYear()
     );
+
+    console.log(this.filtro);
+    
 
     
 
