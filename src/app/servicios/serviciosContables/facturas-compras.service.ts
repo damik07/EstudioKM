90016a86e32ae23509704a5b56cbb6ba266115e6
damik07 <96784561+damik07@users.noreply.github.com>
2023-04-11 10:04:49 -0300

    1 file modified

--- src/app/servicios/serviciosContables/facturas-compras.service.ts
+++ src/app/servicios/serviciosContables/facturas-compras.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn:'root'})
+  providedIn: 'root'
+})
 export class FacturasComprasService {
 
-  facturasCompras?:any=[
-    
+  facturasCompras?: any = [
+    {
+      fecha_factura: "01/01/2023",
+      documento: "01",
+      p_venta: "20",
+      n_desde: "11465",
+      n_hasta: "",
+      cod_autoriz: "01",
+      doc_emisor: "01",
+      n_emisor: "30674549527",
+      denominacion: "01",
+      tc: "01",
+      moneda: "01",
+      neto_gravado: "01",
+      neto_no_gravado: "01",
+      op_exentas: "01",
+      iva: "01",
+      total: "01",
+      cuenta: "01",
+      fecha_imputacion: "01/01/2023",
+      fecha_carga: "01/01/2023"
+    }
+
   ];
 
   agregarFacturasCompras(nuevaFacturas) {
