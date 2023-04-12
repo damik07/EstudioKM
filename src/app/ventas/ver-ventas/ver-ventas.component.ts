4aa2cea463ea3ec17f1f3fe9c47185fcca5dfdab
damik07 <96784561+damik07@users.noreply.github.com>
2023-04-12 10:58:00 -0300

    2 files modified

--- src/app/ventas/ver-ventas/ver-ventas.component.ts
+++ src/app/ventas/ver-ventas/ver-ventas.component.ts
@@ -19,7 +19,45 @@ export class VerVentasComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.formulario = new FormGroup({
+      fecha_comprobante: new FormControl(),
+      tipo_comprobante: new FormControl(),
+      controlador_fiscal: new FormControl(),
+      punto_venta: new FormControl(),
+      n_comprobante: new FormControl(),
+      n_comprobante_registrado: new FormControl(),
+      cant_hojas: new FormControl(),
+      cod_tipo_doc_receptor: new FormControl(),
+      n_documento_receptor: new FormControl(),
+      nombre_receptor: new FormControl(),
+      imp_total_operacion: new FormControl(),
+      imp_total_no_gravado: new FormControl(),
+      imp_neto_gravado: new FormControl(),
+      impuesto_liquidado_iva: new FormControl(),
+      percepcion_no_categorizados: new FormControl(),
+      importe_op_exentas: new FormControl(),
+      importe_pago_cuenta_imp_nac: new FormControl(),
+      importe_perc_ii_bb: new FormControl(),
+      importe_perc_imp_municipales: new FormControl(),
+      importe_imp_internos: new FormControl(),
+      transporte: new FormControl(),
+      tipo_responsable: new FormControl(),
+      cod_moneda: new FormControl(),
+      tipo_cambio: new FormControl(),
+      cod_alicuota_iva: new FormControl(),
+      cod_operacion: new FormControl(),
+      cae: new FormControl(),
+      fecha_venc_cae: new FormControl(),
+      fecha_anulacion_comprobante: new FormControl(),
+      cuenta: new FormControl(),
+      fecha_imputacion: new FormControl()
+    });
   }
+
+  ngDoCheck() {
+
+  }
+
 
   filterData1(formData){           
     // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
