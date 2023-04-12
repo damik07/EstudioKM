96196d397ef6ba671660e9dc18d56d5a0acb9fc3
damik07 <96784561+damik07@users.noreply.github.com>
2023-04-12 12:19:30 -0300

    1 file added

---
+++ src/app/servicios/serviciosContables/asientos-contables.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn:'root'})
+export class AsientosContablesService {
+  asientosContables?:any=[
+
+    {
+      "id": "01",
+      "idTransaccion": 1,      
+      "codificacion": "1.01.01.01",
+      "signoSaldo": -1,
+      "importe": 500000,
+      "fechaMovimiento": "2023-02-01",
+      "fechaCarga": "2023-02-01"
+    },
+    {
+      "id": "01",
+      "idTransaccion": 1,      
+      "codificacion": "1.01.01.01",
+      "signoSaldo": 1,
+      "importe": 500000,
+      "fechaMovimiento": "2023-02-01",
+      "fechaCarga": "2023-02-01"
+    },
+    
+  ];
+
+  agregarAsientoContable(asiento) {
+    this.asientosContables.push(asiento);
+  };
+}
