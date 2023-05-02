de6f0e1781d11803bd7571b4bf4ac0b97ed8256f
damik07 <96784561+damik07@users.noreply.github.com>
2023-05-02 11:49:43 -0300

    2 files modified

--- src/app/asientos/asiento-diario/asiento-diario.component.ts
+++ src/app/asientos/asiento-diario/asiento-diario.component.ts
@@ -12,4 +12,20 @@ export class AsientoDiarioComponent implements OnInit {
   ngOnInit() {
   }
 
+  agregarFila() {
+    const tablaBody = document.getElementById('tablaBody');
+    const nuevaFila = document.createElement('tr');
+
+    for (let i = 0; i < 3; i++) {
+      const nuevaCelda = document.createElement('td');
+      const nuevoInput = document.createElement('input');
+      nuevoInput.type = 'text';
+
+      nuevaCelda.appendChild(nuevoInput);
+      nuevaFila.appendChild(nuevaCelda);
+    }
+
+    tablaBody.appendChild(nuevaFila);
+  }
+
 }
