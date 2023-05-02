a19faf296519d72bec40d58de84a208fc721a101
damik07 <96784561+damik07@users.noreply.github.com>
2023-05-02 12:33:08 -0300

    1 file modified

--- src/app/asientos/asiento-diario/asiento-diario.component.ts
+++ src/app/asientos/asiento-diario/asiento-diario.component.ts
@@ -20,16 +20,61 @@ export class AsientoDiarioComponent implements OnInit {
     const tablaBody = document.getElementById('tablaBody');
     const nuevaFila = document.createElement('tr');
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 2; i++) {
       const nuevaCelda = document.createElement('td');
       const nuevoInput = document.createElement('input');
       nuevoInput.type = 'text';
+      nuevoInput.id = `input${i + 1}`;
+      nuevoInput.addEventListener('keydown', (event) => {
+        if (event.ctrlKey && event.key === 'h') {
+          const target = event.target as HTMLElement;
+        this.mostrarBuscador(target);
+        }
+      });
 
       nuevaCelda.appendChild(nuevoInput);
       nuevaFila.appendChild(nuevaCelda);
     }
 
+    const celdaBoton = document.createElement('td');
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.addEventListener('click', () => {
+      nuevaFila.remove();
+    });
+
+    celdaBoton.appendChild(botonEliminar);
+    nuevaFila.appendChild(celdaBoton);
+
     tablaBody.appendChild(nuevaFila);
+  }
+
+  mostrarBuscador(inputElement: HTMLElement) {
+    // Obtener el valor actual del input
+    if (inputElement instanceof HTMLInputElement) {
+      const valorInput = inputElement.value;
+
+    // Crear un elemento de buscador y configurarlo según tus necesidades
+    const buscador = document.createElement('div');
+    // Agregar estilos, atributos y funcionalidad al buscador
+
+    // Realizar la búsqueda y mostrar los resultados en el buscador
+    const resultados = this.realizarBusqueda(valorInput);
+    this.mostrarResultadosBusqueda(buscador, resultados);
+
+    // Agregar el buscador a la página (por ejemplo, al body)
+    document.body.appendChild(buscador);
+    };
+  }
+
+  realizarBusqueda(valor: string): string[] {
+    // Lógica para realizar la búsqueda y obtener los resultados
+    // Retorna un array de resultados
+    return ['Resultado 1', 'Resultado 2', 'Resultado 3'];
+  }
+
+  mostrarResultadosBusqueda(buscador: HTMLElement, resultados: string[]) {
+    // Lógica para mostrar los resultados en el buscador
   }
 
 }
