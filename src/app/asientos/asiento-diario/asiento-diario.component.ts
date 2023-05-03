8b9390d694d2d829412b29f75e12fd6e8a693f0b
damik07 <96784561+damik07@users.noreply.github.com>
2023-05-03 12:03:12 -0300

    2 files modified

--- src/app/asientos/asiento-diario/asiento-diario.component.ts
+++ src/app/asientos/asiento-diario/asiento-diario.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-asiento-diario',
@@ -16,6 +17,7 @@ export class AsientoDiarioComponent implements OnInit {
   ngOnInit() {
   }
 
+  
   agregarFila() {
     const tablaBody = document.getElementById('tablaBody');
     const nuevaFila = document.createElement('tr');
@@ -25,12 +27,6 @@ export class AsientoDiarioComponent implements OnInit {
       const nuevoInput = document.createElement('input');
       nuevoInput.type = 'text';
       nuevoInput.id = `input${i + 1}`;
-      nuevoInput.addEventListener('keydown', (event) => {
-        if (event.ctrlKey && event.key === 'm') {
-          const target = event.target as HTMLElement;
-        this.mostrarBuscador(target);
-        }
-      });
 
       nuevaCelda.appendChild(nuevoInput);
       nuevaFila.appendChild(nuevaCelda);
@@ -47,40 +43,10 @@ export class AsientoDiarioComponent implements OnInit {
     nuevaFila.appendChild(celdaBoton);
 
     tablaBody.appendChild(nuevaFila);
+    
   }
 
-  mostrarBuscador(inputElement: HTMLElement) {
-    // Obtener el valor actual del input
-    if (inputElement instanceof HTMLInputElement) {
-      const valorInput = inputElement.value;
-
-    // Crear un elemento de buscador y configurarlo según tus necesidades
-    const buscador = document.createElement('div');
-    // Agregar estilos, atributos y funcionalidad al buscador
-
-    // Realizar la búsqueda y mostrar los resultados en el buscador
-    const resultados = this.realizarBusqueda(valorInput);
-    this.mostrarResultadosBusqueda(buscador, resultados);
-
-    // Agregar el buscador a la página (por ejemplo, al body)
-    document.body.appendChild(buscador);
-    };
-  }
-
-  realizarBusqueda(valor: string): string[] {
-    // Lógica para realizar la búsqueda y obtener los resultados
-    // Retorna un array de resultados
-    return ['Resultado 1', 'Resultado 2', 'Resultado 3'];
-  }
-
-  mostrarResultadosBusqueda(buscador: HTMLElement, resultados: string[]) {
-    // Lógica para mostrar los resultados en el buscador
-  }
-
-
-
-
-
+  
 
   agregarFila1() {
     const tablaBody = document.getElementById('tablaBody1');
@@ -91,12 +57,7 @@ export class AsientoDiarioComponent implements OnInit {
       const nuevoInput = document.createElement('input');
       nuevoInput.type = 'text';
       nuevoInput.id = `input${i + 1}`;
-      nuevoInput.addEventListener('keydown', (event) => {
-        if (event.ctrlKey && event.key === 'm') {
-          const target = event.target as HTMLElement;
-        this.mostrarBuscador(target);
-        }
-      });
+      
 
       nuevaCelda.appendChild(nuevoInput);
       nuevaFila.appendChild(nuevaCelda);
@@ -114,5 +75,7 @@ export class AsientoDiarioComponent implements OnInit {
 
     tablaBody.appendChild(nuevaFila);
   }
+
+  
 
 }
