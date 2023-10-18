cddca5ec1dc36afa3c8f968c59cbfe20b1038953
damik07 <96784561+damik07@users.noreply.github.com>
2023-10-18 11:25:45 -0300

    2 files modified

--- src/app/bancos/conciliacion-bancaria/conciliacion-bancaria.component.ts
+++ src/app/bancos/conciliacion-bancaria/conciliacion-bancaria.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FacturasVentasService } from 'src/app/servicios/serviciosContables/facturas-ventas.service';
 
 @Component({
   selector: 'app-conciliacion-bancaria',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConciliacionBancariaComponent implements OnInit {
 
-  constructor() { }
+  data?:any;
+  filtro?:any[];
+  objetoAEditar:any;
+  formulario: FormGroup;
+
+  constructor(private ventas:FacturasVentasService) { }
 
   ngOnInit() {
   }
+
+  filterData1(formData){           
+    // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
+    const ventas = this.ventas.facturasVentas;
+    const datosFiltrados1 = ventas.filter(dato => 
+      new Date(dato.fecha_imputacion).getMonth() >= new Date(formData.startDate1).getMonth()
+    );
+    const dfiltro1 = datosFiltrados1.filter(dato =>
+      new Date(dato.fecha_imputacion).getFullYear() >= new Date(formData.startDate1).getFullYear()
+    );
+    const datosFiltrados2 = dfiltro1.filter(dato => 
+      new Date(dato.fecha_imputacion).getMonth() <= new Date(formData.endDate1).getMonth()
+    );
+    this.filtro = datosFiltrados2.filter(dato =>
+      new Date(dato.fecha_imputacion).getFullYear() <= new Date(formData.endDate1).getFullYear()
+    );
+
+    console.log(this.filtro);
+    
+
+
+  };
 
 }
